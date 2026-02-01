f324d3066cfbfb957a25a6e28ea9b395e99349ad
vanshrathi1928-creator <vanshrathi1928@gmail.com>
2026-02-01 11:56:56 +0530

    commit

---
+++ series 60/index.js
@@ -0,0 +1,24 @@
+console.log("this is a string")
+let a = "vansh";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(a.length)
+
+let name = " vansh ";
+let friend = " salman khan "
+
+console.log("his name is"+name+"and his friend name is" +friend)
+console.log(`his friend name is ${name} and his friend name is${friend}`)
+ 
+
+let b = " vansh rathi"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(0, 10))
+console.log(b.replace("rathi","00001"))
+console.log(b.concat, "vansh", "rathi", "00019")
