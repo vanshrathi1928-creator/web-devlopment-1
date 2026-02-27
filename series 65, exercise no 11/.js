235d98df9ee82a7bb38f8b2c5173c8838a429309
vanshrathi1928-creator <vanshrathi1928@gmail.com>
2026-02-27 19:00:08 +0530

    commit

---
+++ series 65, exercise no 11/.js
@@ -0,0 +1,18 @@
+//with for loop
+let fac = 5
+a = 1 //9  //7
+for (let i=1; i<a; i++){
+    fac+=fac*i
+}
+console.log(fac)
+
+//with reduce function
+// let num = 0;
+
+// for(let i = 0; i<a; i++){
+//     num[1]=a-i
+// }
+// const red=(a,b)=>{
+//     return a*b
+// }
+// console.log(num.reduce(red))
---
+++ series 67, dom 2/script.js
@@ -0,0 +1,6 @@
+console.log("hello world")
+console.log("Hello world")
+
+document.body.firstElementChild 
+document.body.firstElementChild.childNodes 
+document.body.firstElementChild.children
